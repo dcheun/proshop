3b576d8a760ee19821f06ce7313ae39974d04ed5
Danny Cheun <dcheun@gmail.com>
2020-10-07 15:23:50 -0700

    feat: Custom Error Handling
    Add errorMiddleware for customized error handling. It sets 500 error code by default.
    Update server.js to use the middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
 const productRoutes = require("./routes/productRoutes");
@@ -11,11 +12,15 @@ connectDB();
 
 const app = express();
 
-app.use("/api/products", productRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
