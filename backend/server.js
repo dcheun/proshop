b05019eb8e350427072315c6f74f41b19a435008
Danny Cheun <dcheun@gmail.com>
2020-10-07 14:35:18 -0700

    refactor: Update Routes to fetch from db
    Import from mongoose models and create separate routes file for storing routes.
    Add express-async-handler package for better handling exceptions.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDB = require("./config/db");
-const products = require("./data/products");
+
+const productRoutes = require("./routes/productRoutes");
 
 dotenv.config();
 
@@ -10,17 +11,10 @@ connectDB();
 
 const app = express();
 
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is running...");
-});
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
