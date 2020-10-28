c5d9e5cc6c8c04b142150c3b2df7ab5291658494
Danny Cheun <dcheun@gmail.com>
2020-10-28 01:13:16 -0700

    feat: User Auth Endpoint
    Add user controller to handle route logic, user auth, etc.
    Add function to userSchema to check entered pw against user pw.
    Add user routes, eg login.
    Add middleware express.json so it can properly parse body, and user api routes.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   getProducts,
   getProductById,
-} = require("../controllers/productController.js");
+} = require("../controllers/productController");
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
 const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
