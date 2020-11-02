3dd6632df205d334dd1b129f5388d67327c6600b
Danny Cheun <dcheun@gmail.com>
2020-11-02 01:06:34 -0800

    feat: Get Order by ID Endpoint
    Add API endpoint for getting an order by id.
    Add route into orderRoutes.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { addOrderItems } = require("../controllers/orderController");
+const {
+  addOrderItems,
+  getOrderById,
+} = require("../controllers/orderController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
+// Make sure this route is at the bottom.
+router.route("/:id").get(protect, getOrderById);
 
 module.exports = router;
