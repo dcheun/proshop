2b9f8485a8ccf5e4b6e318a1fd76afefcecd0142
Danny Cheun <dcheun@gmail.com>
2020-11-02 22:22:12 -0800

    feat: Update To Paid Endpoint
    Add backend orderController method to update order status to paid in db. Also add in Paypal's return info.
    Add in updateOrderToPaid to order routes.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require("../controllers/orderController");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.route("/").post(protect, addOrderItems);
 // Make sure this route is at the bottom.
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 module.exports = router;
