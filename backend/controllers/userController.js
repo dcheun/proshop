bf6003e190c2c214a8b146d4bf781321b1f6ab29
Danny Cheun <dcheun@gmail.com>
2020-11-06 04:53:35 -0800

    feat: Get User By ID and Update User Endpoints
    Add backend functionality to get user by id and update user by id as admin.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -124,6 +124,46 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    GET user by id
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.isAdmin) {
+      user.isAdmin = req.body.isAdmin;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 module.exports = {
   authUser: authUser,
   getUserProfile: getUserProfile,
@@ -131,4 +171,6 @@ module.exports = {
   updateUserProfile: updateUserProfile,
   getUsers: getUsers,
   deleteUser: deleteUser,
+  getUserById: getUserById,
+  updateUser: updateUser,
 };
