f643f19d44260926795656bc4ec6d3f81a343776
Danny Cheun <dcheun@gmail.com>
2020-11-06 03:29:17 -0800

    feat: Admin User Delete
    Add backend endpoint and routes for deleting user by id.
    Add frontend functionality to delete user - constants, action, reducer.
    Update user list screen to hook up delete button to deleteHandler.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
@@ -85,6 +88,19 @@ export const userListReducer = (state = { users: [] }, action) => {
       return { loading: false, error: action.payload };
     case USER_LIST_RESET:
       return { users: [] };
+    default:
+      return state;
+  }
+};
+
+export const userDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true };
+    case USER_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
