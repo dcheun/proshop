7d8360eec8a8ac6433508e5904f56c2c3f52b045
Danny Cheun <dcheun@gmail.com>
2020-11-06 23:26:24 -0800

    feat: Create and Update Product Endpoints
    Add backend routes for creating and updating products.
    Add sample image for placeholder product image.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,18 @@ const {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } = require("../controllers/productController");
 const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 module.exports = router;
