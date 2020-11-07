0e915cc844c1a8e104e0afef5ae425cb83ccd73a
Danny Cheun <dcheun@gmail.com>
2020-11-07 05:52:26 -0800

    feat: Front End Image Upload
    Add upload file handler for file upload functionality in product edit screen.

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,8 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 // NOTE: If we're using ES6 syntax, we need to do the following line.
-// const __dirname = path.resolve()
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use(notFound);
 
