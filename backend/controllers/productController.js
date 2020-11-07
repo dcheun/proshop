6a8a8f9632b9108968829710f82087906d6a0945
Danny Cheun <dcheun@gmail.com>
2020-11-06 22:08:09 -0800

    feat: Admin Delete Products
    Add backend routes to delete products.
    Add frontend functionality to delete products - constants, actions, reducer.
    Update ProductListScreen, hook up delete action.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,22 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getProducts: getProducts, getProductById: getProductById };
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+module.exports = {
+  getProducts: getProducts,
+  getProductById: getProductById,
+  deleteProduct: deleteProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const {
   getProducts,
   getProductById,
+  deleteProduct,
 } = require("../controllers/productController");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").get(getProducts);
-
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 module.exports = router;
